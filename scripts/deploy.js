03bdecda384161f6c0f3b2244c763ec1e07b9444
DonGuillotine <infect3dlab@gmail.com>
2024-10-01 17:35:35 +0100

    Feat: Deploy to Lisk Sepolia testnet

---
+++ scripts/deploy.js
@@ -0,0 +1,20 @@
+const hre = require("hardhat");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contract with the account:", deployer.address);
+
+  const LudoGame = await hre.ethers.getContractFactory("LudoGame");
+  const ludoGame = await LudoGame.deploy(deployer.address);
+  await ludoGame.waitForDeployment();
+
+  console.log("LudoGame deployed to:", await ludoGame.getAddress());
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
